a647c6dc478c870458322063af9852f0c8ceaeb0
joseph.ryan.l.valdez <joseph.ryan.l.valdez@accenture.com>
2023-01-18 00:24:02 +0800

    init for this

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './Team.css'
 
-const Team = ({ team, id }) => {
+const Team = ({ team, id, addNewChannel, removeChannel, channelCount }) => {
+  const [newChannel, setNewChannel] = useState('')
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  let response;
+
+  const checkChannelName = (event) => {
+    let newChannel = event.target.value;
+    
+    response = team.channels.find(channel => {
+      if(channel.name === newChannel) return true;
+    });
+
+    response = response ? true:false;
+    
+    setIsBtnDisabled(response);
+  }
+
+  const onChangeAddChannel = (event) => {
+    setNewChannel(event.target.value);
+  }
 
   return (
     <div>
@@ -16,10 +35,23 @@ const Team = ({ team, id }) => {
             placeholder='Enter Channel Name'
             className="channel-name-input w-45 px-13"
             data-testid={ 'channel-name-input-' + id }
+            onChange={onChangeAddChannel}
+            onKeyPress={checkChannelName}
+            onKeyUp={checkChannelName}
+            value={newChannel}
           />
           <button 
             className='channel-name-btn x-small w-35 h-30 pa-6 ma-0 ml-6'
             data-testid={ 'add-channel-btn-' + id }
+            onClick={ e=>{
+              addNewChannel(id, {
+                 name: `${newChannel}`,
+                 id: channelCount
+              });
+              setIsBtnDisabled(true);
+              setNewChannel('');
+            }}
+            disabled={isBtnDisabled}
           >
             Add Channel
           </button>
@@ -37,6 +69,9 @@ const Team = ({ team, id }) => {
               <button 
                 data-testid={ 'remove-channel-button-' + id + channel.id }
                 className='icon-only x-small danger ma-0 pa-0'
+                onClick={e=>{
+                  removeChannel(id, channel.id)
+                }}
               >
                 <i className="material-icons">delete</i>
               </button>
--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -4,50 +4,98 @@ import Team from './Team'
 import './TeamList.css'
 
 const TeamList = () => {
-  const teamList = [
-    { name: 'Team1',
-      channels: [
-        { name: 'Channel1',
-          id: 1
-        },
-        { name: 'Channel2',
-          id: 2
-        }
-      ]
-    },
-    { name: 'Team2',
-      channels: [
-        { name: 'Channel1',
-          id: 1
-        },
-        { name: 'Channel2',
-          id: 2
-        }
-      ]
+  const [teamName, setTeamName] = useState('');
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const [teamList, setTeamList] = useState([]);
+  const [channelCount, setChannelCoun] = useState(0);
+
+  const checkTeamName = (event) => {
+    let newTeamName = event.target.value;
+    let response;
+
+    response = teamList.find(team => {
+      if(team.name === newTeamName) return true;
+    });
+
+    response = response ? true:false;
+
+    setIsBtnDisabled(response);
+  }
+
+  const addNewTeam = (e) => {
+    e.preventDefault();
+    let object = {
+      name:teamName,
+      channels:[]
     }
-  ]
+    
+    setTeamList([...teamList, object]);
+    setTeamName('');
+    setIsBtnDisabled(true);
+  }
+
+  const onChangeTeamName = (event) => {
+    setTeamName(event.target.value);
+  }
+
+  const addNewChannel = (teamKey, channelDetails, setChannelAdd) => {
+    let team = [...teamList];
+    let channel = team[teamKey].channels;
+    channel.push(channelDetails)
+    team[teamKey].channel = channel;
+    
+    setTeamList(team);
+    setChannelCoun(channelCount+1)
+  }
+
+  const removeChannel = (teamKey, channelId) => {
+    console.log({channelId});
+    let team = [...teamList];
+    let channels = team[teamKey].channels;
+    let newChannel;
+
+    if (channels && channels.length > 0) {
+      newChannel = channels.filter((chan) => chan.id !== channelId) 
+    }
+    
+    team[teamKey].channels = newChannel;
+    
+    setTeamList(team);
+  }
 
   return (
     <div className='w-50 mx-auto'>
       <div className='card w-35 mt-50 mx-auto px-10 py-15'>
-        <div className='layout-column' data-testid='team-list'>
-          { teamList && teamList.map((team, id) => (
-            <Team
-              key={ id } 
-              id={ id }
-              team={ team } 
-            />
-          ))}
+        <div className='layout-column' data-testid='team-list'>  
+            { teamList.map((team, id) => {
+              return (
+                <Team
+                  key={ id } 
+                  id={ id }
+                  team={ team } 
+                  addNewChannel={addNewChannel}
+                  removeChannel={removeChannel}
+                  channelCount={channelCount}
+                />
+              )
+            })
+          }
         </div>
         <div className='layout-row'>
           <input 
+            value={teamName}
             placeholder='Enter Team Name'
             className='team-list-input w-75'
             data-testid='team-name-input'
+            onChange={onChangeTeamName}
+            onKeyPress={checkTeamName}
+            onKeyUp={checkTeamName}
           />
           <button 
             className='team-list-btn x-small w-35 h-30 pa-6 ma-0 ml-6'
             data-testid='add-team-btn'
+            onClick={addNewTeam}
+            disabled={isBtnDisabled}
           >
             Add Team
           </button>
